589cbdfe2c4248a987402f6ab0abe8ea3b203f2a
SSP-노예원 <SSP-yewon@hwk.net>
2021-11-10 03:34:01 +0900

    sap

--- get.py
+++ get.py
@@ -1,17 +1,35 @@
 import os
 import requests
+import json
 
 
-
-print(os,path.isfile("ryw.html"))
+print(os.path.isfile("ryw.html"))
 file=open('ryw.html','w',encoding='UTF-8')
 
 file.write("<html><head><title>테스트</title></head><body>Hello CICD</body></html>") 
 file.close()
 
 
-URL='127.0.0.1:5000'
-response=requests.get(URL)
-response.status_code
+value='1234'
+params={'code':value}
+URL='http://3.112.213.226:8080'
+response=requests.get(URL, params=params)
+print(response.status_code)
+print(response.url)
+#print(response.json())
+
+
+response=requests.post(URL, data=params)
+print(response.status_code)
+print(response.url)
+#print(response.text)
+
+
+
+#params={'key':'value'}
+#stream = os.popen('curl -s ifconfig.me')
+#URL = stream.read()
+#reponse=requests.get("http://"+URL+":8080")
+#print(reponse.status_code)
 
 
