903a5a5c075b87f02131967345efd121b49f6603
SSP-노예원 <SSP-yewon@hwk.net>
2021-11-10 14:15:23 +0900

    Test

--- get.py
+++ get.py
@@ -3,14 +3,6 @@ import os
 import requests
 #import json
 
-
-print(os.path.isfile("ryw.html"))
-file=open('ryw.html','w',encoding='UTF-8')
-
-file.write("<html><head><title>테스트</title></head><body>Hello CICD</body></html>") 
-file.close()
-
-
 value='1234'
 params={'code':value}
 URL='http://3.112.213.226:8080/test/yw.html'
@@ -26,7 +18,7 @@ print(response.url)
 print(response.text)
 
 
-
+#####
 #params={'key':'value'}
 #stream = os.popen('curl -s ifconfig.me')
 #URL = stream.read()
